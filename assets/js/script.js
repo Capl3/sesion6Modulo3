2c27f3aaec70c9d5236fe7e03423edcaff87d627
Rodrigo Miranda <rodrigomiranda023@gmail.com>
2023-04-27 19:46:43 -0400

    base proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* Arrelgos */
+/* Son una lista de datos */
+/* Tienne un largo dinamico */
+/* Se recorren con un for o con cualquier ciclo */
+/* Saer definen entre corchetes y cad elemnto va separado por coma */
+/* Tienen una posicion y un indice */
+/* Pueden tener un sublemento o mas */
+/* Podemos guardar funciones, objetos o distintas estructueras de datos */
+/* Metodos (funciones) */
+
