d86ef1dc7c29a2b7d687ed46ffa197a2d9461435
Rodrigo Miranda <rodrigomiranda023@gmail.com>
2023-04-27 20:03:54 -0400

    Consumo de API

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,25 @@
 /* Podemos guardar funciones, objetos o distintas estructueras de datos */
 /* Metodos (funciones) */
 
+
+/* Los arreglos se definen desde cero y el indice */
+/* Métodos de arreglos */
+/* Push: agrega un elemto a la última posición del arreglo */
+/* Unshift: agrega un elemto a la primera posición del arreglo */
+/* Shift: elimina un elemto  a la primera posiscion del arrelgo */
+/* Pop: elimina un elemento a la última posición del arreglo */
+/* Splice: elimina un elemento o varios desde un elemnto indicado */
+
+/* Endpoint: https://pokeapi.co/api/v2/pokemon/ */
+function consumoAPI() {
+    $.ajax({
+        type: "get",
+        url: "https://pokeapi.co/api/v2/pokemon/",
+        success: function (response) {
+            console.log(response)
+
+        }
+    });
+}
+
+consumoAPI();
